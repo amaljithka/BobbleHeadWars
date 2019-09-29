5915d8e3c44e43a5813bdb4ef747b93dfcdc1a35
Amaljith Kavungalparambil Ayyappan <55007565+amaljithka@users.noreply.github.com>
2019-09-28 21:16:27 -0400

    Created the Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] spawnPoints;
+    public GameObject alien;
+    public int maxAliensOnScreen;
+    public int totalAliens;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    public int aliensPerSpawn;
+
+    private int aliensOnScreen = 0;
+    private float generatedSpawnTime = 0;
+    private float currentSpawnTime = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
