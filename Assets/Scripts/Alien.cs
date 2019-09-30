783617898bda233df2870a3348a30c729f129cd6
Amaljith Kavungalparambil Ayyappan <55007565+amaljithka@users.noreply.github.com>
2019-09-30 08:52:43 -0400

    Updated NavMesh and Created Agents

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+ 
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.destination = target.position;
+        }
+    }
+}
+  
