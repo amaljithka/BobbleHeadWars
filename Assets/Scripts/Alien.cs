12bdfe8983b5755df9060bf713885690ea67033d
Amaljith Kavungalparambil Ayyappan <55007565+amaljithka@users.noreply.github.com>
2019-10-04 20:32:08 -0400

    Added first animation

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -7,6 +7,8 @@ public class Alien : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent agent;
+    public float navigationUpdate;
+    private float navigationTime = 0; 
 
     void Start()
     {
@@ -16,10 +18,17 @@ public class Alien : MonoBehaviour
  
     void Update()
     {
-        if (target != null)
+        navigationTime += Time.deltaTime;
+        if (navigationTime > navigationUpdate)
         {
             agent.destination = target.position;
+            navigationTime = 0;
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
   
